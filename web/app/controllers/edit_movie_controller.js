37126cc2667c593e519cede90c59c45789f0a672
madhaanr <marko.haanranta@helsinki.fi>
2015-04-27 12:48:28 +0300

    Now 44 ok

--- web/app/controllers/edit_movie_controller.js
+++ web/app/controllers/edit_movie_controller.js
@@ -1,14 +1,16 @@
 MoviesApp.controller('EditMovieController', function ($scope, FirebaseService, $location, $routeParams) {
     $scope.editedMovie = {};
+
     var done = function (movie) {
         $scope.movie = movie;
-    }
+        $scope.editedMovie = $scope.movie;      
+    };
 
     FirebaseService.getMovie($routeParams.KEY, done);
 
-    $scope.editMovie = function (movie) {
-        if (movie.title !== '' && movie.director !== '' && movie.release !== '' && movie.description !== '') {
-            FirebaseService.editMovie(movie);
+    $scope.editMovie = function () {
+        if ($scope.editedMovie.title !== '' && $scope.editedMovie.director !== '' && $scope.editedMovie.release !== '' && $scope.editedMovie.description !== '') {
+            FirebaseService.editMovie($scope.editedMovie);
             $location.path('/movies');
         }
     };
