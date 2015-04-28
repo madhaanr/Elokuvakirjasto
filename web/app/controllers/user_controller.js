8ba4f4f09efc576cdeefbabb110dd4ac14484372
madhaanr <marko.haanranta@helsinki.fi>
2015-04-28 11:46:00 +0300

    46 ok.

--- web/app/controllers/user_controller.js
+++ web/app/controllers/user_controller.js
@@ -1,25 +1,27 @@
-MoviesApp.controller('UserController', function($scope, $location, AuthenticationService){
-  
-  $scope.logIn = function(){
-    AuthenticationService.logUserIn($scope.email, $scope.password)
-    .then(function(){
-      $location.path('/movies');
-    })
-    .catch(function(){
-      $scope.message = 'Väärä sähköpostiosoite tai salasana!'
-    });
-  }
+MoviesApp.controller('UserController', function ($scope, $location, AuthenticationService,$rootScope) {
 
-  $scope.register = function(){
-    AuthenticationService.createUser($scope.newEmail, $scope.newPassword)
-    .then(function(){
-      AuthenticationService.logUserIn($scope.newEmail, $scope.newPassword)
-      .then(function(){
-        $location.path('/movies');
-      });
-    })
-    .catch(function(){
-      $scope.message = 'Tapahtui virhe! Yritä uudestaan';
-    });
-  }
+    $scope.logIn = function () {
+        AuthenticationService.logUserIn($scope.email, $scope.password)
+                .then(function () {
+                    $rootScope.userLoggedIn = AuthenticationService.getUserLoggedIn();
+                    $location.path('/movies');
+                })
+                .catch(function () {
+                    $scope.message = 'Väärä sähköpostiosoite tai salasana!'
+                });
+    }
+
+    $scope.register = function () {
+        AuthenticationService.createUser($scope.newEmail, $scope.newPassword)
+                .then(function () {
+                    AuthenticationService.logUserIn($scope.newEmail, $scope.newPassword)
+                            .then(function () {
+                                $rootScope.userLoggedIn = AuthenticationService.getUserLoggedIn();
+                                $location.path('/movies');
+                            });
+                })
+                .catch(function () {
+                    $scope.message = 'Tapahtui virhe! Yritä uudestaan';
+                });
+    }
 });
